94e3380c2d6978a5286506d3cb20d0dbcce5d509
Raul Cruz <raul.cruz@zyncro.com>
2016-04-13 15:29:02 +0200

    Add aliases and functions

---
+++ git.plugin.zsh
@@ -0,0 +1,56 @@
+# gcheckout - checkout local|remote branch
+gcb() {
+    local branches branch
+    branches=$(git branch --all | grep -v HEAD) && 
+    branch=$(echo "$branches" | fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+    git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
+# gshow - commit browser
+gshow() {
+    git log --graph --color=always \
+        --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+    fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+        --bind "ctrl-m:execute: (grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF' {} FZF-EOF"
+}
+
+# gcshow - get git commit sha
+gcshow() {
+    git rebase -i `local commits commit
+    commits=$(git log --color=always --pretty=oneline --abbrev-commit --reverse) &&
+    commit=$(echo "$commits" | fzf --tac +s +m -e --ansi --reverse) &&
+    echo -n $(echo "$commit" | sed "s/ .*//")`
+}
+
+# grebase - interactive rebase
+alias grebase='git rebase -i `gcshow`'
+
+# fstash - easier way to deal with stashes
+# # type fstash to get a list of your stashes
+# # enter shows you the contents of the stash
+# # ctrl-d shows a diff of the stash against your current HEAD
+# # ctrl-b checks the stash out as a branch, for easier merging
+gstash() {
+    local out q k sha
+    while out=$(
+        git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
+        fzf --ansi --no-sort --query="$q" --print-query \
+            --expect=ctrl-d,ctrl-b);
+    do
+        mapfile -t out <<< "$out"
+        q="${out[0]}"
+        k="${out[1]}"
+        sha="${out[-1]}"
+        sha="${sha%% *}"
+        [[ -z "$sha" ]] && continue
+        if [[ "$k" == 'ctrl-d' ]]; then
+            git diff $sha
+        elif [[ "$k" == 'ctrl-b' ]]; then
+            git stash branch "stash-$sha" $sha
+            break;
+        else
+            git stash show -p $sha
+        fi
+    done
+}
+
