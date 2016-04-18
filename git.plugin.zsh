a9b2ce78600a0309187edf682cc69bc96d201c07
Raúl Cruz <raul.cruz@zyncro.com>
2016-04-18 11:09:19 +0200

    Fix gshow function

--- git.plugin.zsh
+++ git.plugin.zsh
@@ -11,8 +11,13 @@ function gshow() {
     git log --graph --color=always \
         --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
     fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
-        --bind "ctrl-m:execute: (grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF' {} FZF-EOF"
+        --bind "ctrl-m:execute:
+                  (grep -o '[a-f0-9]\{7\}' | head -1 |
+                  xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                  {}
+FZF-EOF"
 }
+
 
 # gcshow - get git commit sha
 function gcshow() {
