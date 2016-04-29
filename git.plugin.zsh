3bc3b70537bf9b8b955dccb9c4566b48f859e922
Raúl Cruz <raul.cruz@zyncro.com>
2016-04-29 11:14:38 +0200

    gcheckout allows arguments

--- git.plugin.zsh
+++ git.plugin.zsh
@@ -1,9 +1,13 @@
 # gcheckout - checkout local|remote branch
 function gcheckout() {
-    local branches branch
-    branches=$(git branch --all | grep -v HEAD) &&
-    branch=$(echo "$branches" | fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-    git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+    if [[ -n $1 ]] then
+        git checkout -b "$@"
+    else
+        local branches branch
+        branches=$(git branch --all | grep -v HEAD) &&
+        branch=$(echo "$branches" | fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+        git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+    fi
 }
 
 # gshow - commit browser
@@ -18,7 +22,7 @@ function gshow() {
 FZF-EOF"
 }
 
-# gcshow - get git commit sha
+# gcshow - get git commit SHA-1
 function gcshow() {
     local commits commit
     commits=$(git log --color=always --pretty=oneline --abbrev-commit --reverse) &&
@@ -45,33 +49,3 @@ function gdelete() {
         branch=$(echo "$branches" | fzf-tmux -d 15 +s +m) &&
         git branch -d $(echo "$branch" | sed "s/.* //")
 }
-
-# fstash - easier way to deal with stashes
-# # type fstash to get a list of your stashes
-# # enter shows you the contents of the stash
-# # ctrl-d shows a diff of the stash against your current HEAD
-# # ctrl-b checks the stash out as a branch, for easier merging
-function gstash() {
-    local out q k sha
-    while out=$(
-        git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
-        fzf --ansi --no-sort --query="$q" --print-query \
-            --expect=ctrl-d,ctrl-b);
-    do
-        mapfile -t out <<< "$out"
-        q="${out[0]}"
-        k="${out[1]}"
-        sha="${out[-1]}"
-        sha="${sha%% *}"
-        [[ -z "$sha" ]] && continue
-        if [[ "$k" == 'ctrl-d' ]]; then
-            git diff $sha
-        elif [[ "$k" == 'ctrl-b' ]]; then
-            git stash branch "stash-$sha" $sha
-            break;
-        else
-            git stash show -p $sha
-        fi
-    done
-}
-
